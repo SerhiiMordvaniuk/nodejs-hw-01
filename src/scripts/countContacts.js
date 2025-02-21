ea2096afc43d223da0b12dc9244098fc3fa9a603
Serhii Mordvaniuk <sm636332@gmail.com>
2025-02-21 21:36:56 +0100

    done

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,12 @@
-export const countContacts = async () => {};
+import { getAllContacts } from './getAllContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const contacts = await getAllContacts();
+    return contacts.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await countContacts());
