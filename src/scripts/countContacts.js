36601d46db1fad2218d384dd3722e267d4a2f3db
Serhii Mordvaniuk <sm636332@gmail.com>
2025-02-22 08:13:08 +0100

    fixed counter and writeContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,8 +1,8 @@
-import { getAllContacts } from './getAllContacts.js';
+import { readContacts } from '../utils/readContacts.js';
 
 export const countContacts = async () => {
   try {
-    const contacts = await getAllContacts();
+    const contacts = await readContacts();
     return contacts.length;
   } catch (error) {
     console.log(error);
